842c7aaf42aa13b19818a9dd2b0921d192328600
RadadiyaBrij <radadiyabrij43@gmail.com>
2026-05-18 11:32:39 +0530

    fix: validate duedate before task creation

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -26,7 +26,7 @@ export const createTask = async (req, res) => {
 
     // fetch details for task from request body
     const { title, description, tags, priority, status, dueDate } = req.body;
-    if (!title || !priority || !status) {
+    if (!title || !priority || !status || !dueDate) {
       return res
         .status(400)
         .json({ success: false, message: "Please enter all the details" });
